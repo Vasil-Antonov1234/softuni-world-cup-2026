4db80d6806a43f63a0ed5deaeb5281c05129f122
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-07-28 15:10:29 +0300

    Add create match and get data

--- src/controllers/matchController.js
+++ src/controllers/matchController.js
@@ -4,6 +4,14 @@ const matchController = Router();
 
 matchController.get("/create", (req, res) => {
     res.render("match/create");
+});
+
+matchController.post("/create", async (req, res) => {
+    const matchData = req.body;
+
+    console.log(matchData)
+
+    res.redirect("/");
 })
 
 export default matchController;
